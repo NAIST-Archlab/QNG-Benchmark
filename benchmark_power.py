60ba5b88242e0e8ec411bb33b1805ec90a7822f2
Vu Tuan Hai <43202025+vutuanhai237@users.noreply.github.com>
2024-06-25 09:47:45 +0900

    Update

--- benchmark_power.py
+++ benchmark_power.py
@@ -5,31 +5,31 @@ from qoop.core.state import specific
 from qoop.backend import constant
 import time
 
-num_qubits = int(input('Enter number of qubits: ')
+num_qubits = int(input('Enter number of qubits: '))
 num_layers = 1
 state = np.random.uniform(low = 0, high = 2*np.pi, size = 2**num_qubits)
 
-optimizers = ['sgd', 'adam', 'qng_fubini_study', 'qng_qfim', 'qng_adam']
+optimizer = str(input('Enter optimizer: '))
+num_steps = int(input('Enter num_steps: '))
 
 times = []
-num_steps = 1
-for optimizer in optimizers:
-    start = time.time()
-    compiler = QuantumCompilation(
-        u = g2gn(num_qubits, num_layers),
-        vdagger = specific(state).inverse(),
-        optimizer = optimizer,
-        metrics_func = [
-            'loss_basic', 
-            'compilation_trace_fidelities'
-        ]
-    )
 
-    compiler.fit(
-        num_steps = num_steps, 
-    )
-    #plot figure
-    times.append(time.time() - start)
+start = time.time()
+compiler = QuantumCompilation(
+    u = g2gn(num_qubits, num_layers),
+    vdagger = specific(state).inverse(),
+    optimizer = optimizer,
+    metrics_func = [
+        'loss_basic', 
+        'compilation_trace_fidelities'
+    ]
+)
+
+compiler.fit(
+    num_steps = num_steps, 
+)
+#plot figure
+times.append(time.time() - start)
 print(f'num_qubits: {num_qubits}')
 # np.savetxt(f'times_{num_qubits}.txt', np.round(np.array(times)/num_steps, 4))
 print(np.round(np.array(times)/num_steps, 4))
