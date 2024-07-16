c1c56c999eb6960db49580387c21bd13df91dd46
Vu Tuan Hai <43202025+vutuanhai237@users.noreply.github.com>
2024-07-16 17:16:07 +0900

    Update

---
+++ constant.py
@@ -0,0 +1,6 @@
+
+optimizers_name = ['GD', 'Adam', 'QNG-QFSM-GD', 'QNG-QFIM-GD', 'QNG-QFIM-Adam']
+line_styles = ['solid', 'dotted', 'dashed', 'dashdot', 'dotted']
+colors = ['black', 'black', 'red', 'red', 'red']
+colorss = ['black', 'blue', 'red', 'orange', 'yellow']
+ansatzs = ['g2gn', 'zxz_layer', 'Wchain_zxz', 'Walternating_zxz']
