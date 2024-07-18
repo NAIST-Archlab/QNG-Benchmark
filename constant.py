ef451386bab396c3b4283a70f668b3e623e02a52
Vu Tuan Hai <43202025+vutuanhai237@users.noreply.github.com>
2024-07-18 15:49:45 +0900

    Lack of g2gn time

--- constant.py
+++ constant.py
@@ -3,4 +3,9 @@ optimizers_name = ['GD', 'Adam', 'QNG-QFSM-GD', 'QNG-QFIM-GD', 'QNG-QFIM-Adam']
 line_styles = ['solid', 'dotted', 'dashed', 'dashdot', 'dotted']
 colors = ['black', 'black', 'red', 'red', 'red']
 colorss = ['black', 'blue', 'red', 'orange', 'yellow']
-ansatzs = ['g2gn', 'zxz_layer', 'Wchain_zxz', 'Walternating_zxz']
+ansatzs = ['g2gn', 'zxz_layer', 'Wchain_zxz', 'zxz_WalternatingCNOT']
+ansatzs_name = [
+    '$|g_2 g_n \rangle $', 
+    '$|W_{\text{chain}}+ZXZ \rangle $', 
+    '$|ZXZ+W_{\text{alternating}}\rangle $',
+    '$|ZXZ \rangle $']
