00e652ad3980221bfce545e9450982c4d9b3028a
Vu Tuan Hai <43202025+vutuanhai237@users.noreply.github.com>
2024-08-01 10:55:08 +0900

    Move scripts to right folder
    Delete useless files

--- constant.py
+++ constant.py
@@ -2,7 +2,7 @@
 optimizers_name = ['GD', 'Adam', 'QNG-QFSM-GD', 'QNG-QFIM-GD', 'QNG-QFIM-Adam']
 line_styles = ['solid', 'dotted', 'dashed', 'dashdot', 'dotted']
 colors = ['black', 'black', 'red', 'red', 'red']
-colorss = ['black', 'blue', 'red', 'orange', 'yellow']
+colorss = ['black', 'blue', 'red', 'orange', 'violet']
 ansatzs = ['g2gn', 'zxz_layer', 'Wchain_zxz', 'zxz_WalternatingCNOT']
 ansatzs_name = [
     '$|g_2 g_n \rangle $', 
